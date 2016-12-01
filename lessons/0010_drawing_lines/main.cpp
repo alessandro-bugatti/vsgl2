f7e5c36e8854e4a072a25c3688d75290adeb57d0
alessandro-bugatti <alessandro.bugatti@gmail.com>
2016-12-01 22:47:25 +0100

    Add lesson about drawing lines

---
+++ lessons/0010_drawing_lines/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "../../vsgl2.h"
+
+using namespace std;
+using namespace vsgl2;
+using namespace vsgl2::general;
+using namespace vsgl2::video;
+using namespace vsgl2::utils;
+
+int main(int argc, char* argv[]) {
+    //init the library
+    init();
+    //create the window and show it
+    set_window(512,512,"Vsgl2 line drawing");
+    //draw a line from the top-left corner to
+    //the bottom-right corner of the screen
+    draw_line(0,0,get_window_width(),get_window_height(), Color(255,0,0,255));
+    //update the screen
+    update();
+    //the program waits for any button to be pressed
+    wait_for_button_pressed();
+    //close the library and clean up everything
+    close();
+    return 0;
+}
