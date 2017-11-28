ae7b684a8fee7e1c7b478cf04a82583ad3922c44
alessandro-bugatti <alessandro.bugatti@gmail.com>
2017-11-28 11:01:13 +0100

    Improve lesson on drawing lines

--- lessons/0010_drawing_lines/main.cpp
+++ lessons/0010_drawing_lines/main.cpp
@@ -14,7 +14,11 @@ int main(int argc, char* argv[]) {
     set_window(512,512,"Vsgl2 line drawing");
     //draw a line from the top-left corner to
     //the bottom-right corner of the screen
-    draw_line(0,0,get_window_width(),get_window_height(), Color(255,0,0,255));
+    draw_line(0, //top-left corner
+              0,
+              get_window_width() - 1, //bottom-right corner
+              get_window_height() - 1,
+              Color(255,0,0,255)); //red line
     //update the screen
     update();
     //the program waits for any button to be pressed
