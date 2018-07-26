aa5d10de94efde9cdb18964478e93cdd9b22ba05
alessandro-bugatti <alessandro.bugatti@gmail.com>
2018-07-26 14:53:09 +0200

    Update documentation at 7/26

--- search/all_73.js
+++ search/all_73.js
@@ -1,5 +1,6 @@
 var searchData=
 [
+  ['set_5fbackground_5fcolor',['set_background_color',['../namespacevsgl2_1_1general.html#a9ba96379c1e1112fab09f523101b020a',1,'vsgl2::general']]],
   ['set_5fwindow',['set_window',['../namespacevsgl2_1_1general.html#a59876a39c5fccd5f8b854c77f4c5c1e6',1,'vsgl2::general']]],
   ['sounds',['sounds',['../namespacevsgl2.html#a677cbe4d16dbf5978ccc6e5c6aba5450',1,'vsgl2']]],
   ['stop_5fmusic',['stop_music',['../namespacevsgl2_1_1audio.html#a3ce6129e7ea65b8a896668d110fae950',1,'vsgl2::audio']]]
---
+++ search/variables_62.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['background_5fcolor',['background_color',['../namespacevsgl2.html#a3af4612678da78558adccf47e17164bb',1,'vsgl2']]]
+];
