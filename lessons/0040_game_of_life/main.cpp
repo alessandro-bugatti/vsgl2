37184e9f3ea61647e0895c61bbf43f68cdf84327
alessandro-bugatti <alessandro.bugatti@gmail.com>
2019-02-01 19:06:09 +0100

    Add Game of Life program and documentation

--- lessons/0040_game_of_life/main.cpp
+++ lessons/0040_game_of_life/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <ctime>
 #include "../../vsgl2.h"
 
 using namespace std;
@@ -8,51 +9,94 @@ using namespace vsgl2::general;
 using namespace vsgl2::video;
 using namespace vsgl2::utils;
 
-const int M = 50;
+const int DIM = 64;
+const int CELL_DIMENSION = 8;
 
-void draw_data(float v1[], float v2[], int n, int y, int r)
+int life[DIM][DIM];
+int temp[DIM][DIM];
+
+
+void start(int m[][DIM])
 {
-    int i;
-    float delta = (float)get_window_width()/(n - 1);
-    float max_value = *max_element(v1, v1 + n);
-    float min_value = *min_element(v1, v1 + n);
-    float range = max_value - min_value;
-
-    for (i = 0; i < n-1; i++)
+    for (int i = 0; i < DIM; i++)
+        for (int j = 0; j < DIM; j++)
+            if (rand()%6 == 0)
+                m[i][j] = 1;
+            else
+                m[i][j] = 0;
+    //Put the border cells to zero
+    for (int i = 0; i < DIM; i++)
     {
-        int ya = y - (v1[i] - min_value)/range*r;
-        int yb = y - (v1[i+1] - min_value)/range*r;
-        draw_line(delta*i, ya, delta*(i+1), yb, Color(255,0,0,255));
-    }
-    for (i = 0; i < n-1; i++)
-    {
-        int ya = y - (v2[i] - min_value)/range*r;
-        int yb = y - (v2[i+1] - min_value)/range*r;
-        draw_line(delta*i, ya, delta*(i+1), yb, Color(0,0,255,255));
+        m[0][i] = 0;
+        m[i][0] = 0;
+        m[DIM - 1][i] = 0;
+        m[i][DIM - 1] = 0;
     }
 }
 
-int main(int argc, char* argv[]) {
-    float data[M], filtered_data[M];
-    int i;
-    for (i = 0; i < M; i++)
-        data[i] = 5 + rand()%10/10.0;
-    //data[20] = 7;
-    for (i = 1; i < M-1; i++)
-        filtered_data[i] = (data[i] +
-        data[i+1] + data[i-1])/3;
-    filtered_data[0] = (2*data[0] + data[1])/3;
-    filtered_data[M-1] = (2*data[M-1] + data[M-2])/3;
+void draw(int m[][DIM])
+{
+    for (int i = 0; i < DIM; i++)
+        for (int j = 0; j < DIM; j++)
+            if (m[i][j] == 1)
+                draw_filled_rect(i*CELL_DIMENSION, j*CELL_DIMENSION,
+                                 CELL_DIMENSION - 1, CELL_DIMENSION - 1,
+                                 Color(255,0,0,255));
+}
+
+int neighbours(int m[][DIM], int r, int c)
+{
+    int counter = 0;
+    for (int i = r-1; i < r - 1 + 3; i++)
+        for (int j = c-1; j < c - 1 + 3; j++)
+            counter += m[i][j];
+    return counter - m[r][c];
+}
+
+void evolve(int m[][DIM])
+{
+    for (int i = 1; i < DIM-1; i++)
+        for (int j = 1; j < DIM-1; j++)
+        {
+            int n = neighbours(m, i, j);
+            //Any live cell with fewer than two live neighbors dies,
+            //as if by underpopulation.
+            if (m[i][j] == 1 && n < 2)
+                temp[i][j] = 0;
+            //Any live cell with two or three live neighbors lives
+            //on to the next generation.
+            if (m[i][j] == 1 && (n == 2 || n == 3))
+                temp[i][j] = 1;
+            //Any live cell with more than three live neighbors dies,
+            //as if by overpopulation.
+            if (m[i][j] == 1 && n > 3)
+                temp[i][j] = 0;
+            //Any dead cell with exactly three live neighbors
+            //becomes a live cell, as if by reproduction.
+            if (m[i][j] == 0 && n == 3)
+                temp[i][j] = 1;
+        }
+    for (int i = 1; i < DIM-1; i++)
+        for (int j = 1; j < DIM-1; j++)
+            m[i][j] = temp[i][j];
+}
+
+int main(int argc, char* argv[])
+{
     //init the library
     init();
     //create the window and show it
-    set_window(756,512,"Vsgl2 graphics drawing");
-    draw_data(data, filtered_data, M, get_window_height(),get_window_height());
-    //update the screen
-    update();
-    //the program waits for any button to be pressed
-    wait_for_button_pressed();
-    //close the library and clean up everything
+    set_window(DIM*CELL_DIMENSION, DIM*CELL_DIMENSION,"Vsgl2 Game of Life");
+    srand(time(NULL));
+    start(life);
+    while(!done())
+    {
+        draw(life);
+        update();
+        evolve(life);
+        delay(100);
+    }
+
     close();
     return 0;
 }
--- lessons/0040_game_of_life/readme.md
+++ lessons/0040_game_of_life/readme.md
@@ -1,2 +1,41 @@
 # Game of Life
-The classical [Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) is a sort of mathematical "game" invented by John Conway in the seventies, that it will used to show how to use a matrix to record information about the image to be displayed.
+The classical [Game of Life](https://en.wikipedia.org/wiki/Conway%27s_Game_of_Life) is a sort of mathematical "game" invented by John Conway in the seventies, that it will be used to show how to record information inside a matrix about the image to be displayed.
+
+In a nutshell, there is a NxN matrix with 0 and 1s in each cell, where 0 mean *dead* and 1 mean *alive* (sometimes they are referred as *unpopulated* and *populated*). This matrix evolves with discrete steps, updating every cell at each step using these simple rules:
+- Any live cell with fewer than two live neighbors dies, as if by underpopulation.
+- Any live cell with two or three live neighbors lives on to the next generation.
+- Any live cell with more than three live neighbors dies, as if by overpopulation.
+- Any dead cell with exactly three live neighbors becomes a live cell, as if by reproduction.
+
+As a result, there is a feeling of moving things, because of the continuing change in cell values, as it can see in the below video
+
+[![YouTube video on Game of Life](http://img.youtube.com/vi/OWfAzvJidVQ/0.jpg)](http://www.youtube.com/watch?v=OWfAzvJidVQ "YouTube video on Game of Life")
+
+The code is quite simple, there are four functions that done all the job:
+
+1. ```void start(int m[][DIM])```: populate all the matrix with 0 and 1s, using the the `rand()` function to choose between 0 and 1, with a ratio of 6 to 1, to avoid to overpopulate the matrix
+2. ```void draw(int m[][DIM])```: put the entire matrix to the screen with the ```draw_filled_rect``` function. The coordinates of each cell depends on its *i, j* position, multiplicated for **CELL_DIMENSION**, in this case 8, to have cell that are clearly visible on the screen. Only cells with 1 inside are put on the screen, red coloured.
+3. ```void evolve(int m[][DIM])```: this is the main function, which is in charge of evolving the matrix state using the rules explained above. 
+In particular there are four *if*, each one matching one rule
+```c
+int n = neighbours(m, i, j);
+//Any live cell with fewer than two live neighbors dies, 
+//as if by underpopulation.
+if (m[i][j] == 1 && n < 2)
+	temp[i][j] = 0;
+//Any live cell with two or three live neighbors lives 
+//on to the next generation.
+if (m[i][j] == 1 && (n == 2 || n == 3))
+	temp[i][j] = 1;
+//Any live cell with more than three live neighbors dies, 
+//as if by overpopulation.
+if (m[i][j] == 1 && n > 3)
+	temp[i][j] = 0;
+//Any dead cell with exactly three live neighbors 
+//becomes a live cell, as if by reproduction.
+if (m[i][j] == 0 && n == 3)
+	temp[i][j] = 1;
+```
+4. ```int neighbours(int m[][DIM], int r, int c)```: a little utility function to count how many neighbours cells are near to cell **m[r][c]**
+
+Using all these functions is now easy to implement the main loop, drawing and evolving the state at each step.
