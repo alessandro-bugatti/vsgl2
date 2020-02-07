729af49e4f29c0cdd0f8d2f5d1442dd2cad49eb0
alessandro-bugatti <alessandro.bugatti@gmail.com>
2020-02-07 10:11:53 +0100

    Add first version of a ChangeLog file

--- version.h
+++ version.h
@@ -2,32 +2,32 @@
 #define VERSION_H
 
 namespace AutoVersion{
-
+	
 	//Date Version Types
 	static const char DATE[] = "07";
 	static const char MONTH[] = "02";
 	static const char YEAR[] = "2020";
 	static const char UBUNTU_VERSION_STYLE[] =  "20.02";
-
+	
 	//Software Status
 	static const char STATUS[] =  "Alpha";
 	static const char STATUS_SHORT[] =  "a";
-
+	
 	//Standard Version Type
 	static const long MAJOR  = 0;
 	static const long MINOR  = 1;
-	static const long BUILD  = 129;
-	static const long REVISION  = 678;
-
+	static const long BUILD  = 131;
+	static const long REVISION  = 682;
+	
 	//Miscellaneous Version Types
-	static const long BUILDS_COUNT  = 269;
-	#define RC_FILEVERSION 0,1,129,678
-	#define RC_FILEVERSION_STRING "0, 1, 129, 678\0"
-	static const char FULLVERSION_STRING [] = "0.1.129.678";
-
+	static const long BUILDS_COUNT  = 275;
+	#define RC_FILEVERSION 0,1,131,682
+	#define RC_FILEVERSION_STRING "0, 1, 131, 682\0"
+	static const char FULLVERSION_STRING [] = "0.1.131.682";
+	
 	//These values are to keep track of your versioning state, don't modify them.
 	static const long BUILD_HISTORY  = 29;
-
+	
 
 }
 #endif //VERSION_H
