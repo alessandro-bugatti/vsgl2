aa91e7687955f0eccdb797b0c476101cfcf79fe2
alessandro-bugatti <alessandro.bugatti@gmail.com>
2021-06-23 15:59:26 +0200

    Add horizontal orientation to the character

--- lessons/0621_moving_character/main.cpp
+++ lessons/0621_moving_character/main.cpp
@@ -8,6 +8,8 @@ using namespace vsgl2::general;
 using namespace vsgl2::video;
 using namespace vsgl2::utils;
 using namespace vsgl2::sprite;
+using namespace vsgl2::io;
+
 
 int main(int argc, char* argv[]) {
     //init the library
@@ -16,14 +18,31 @@ int main(int argc, char* argv[]) {
     set_window(512,512,"Vsgl2 moving character");
 
     //main loop
-    start_animation("images/templerun_run.png",0,200,0,9,100,0);
+    int hero = start_animation("images/templerun_run.png",0,200,0,9,100,0);
+    enum Orientation {
+        LEFT,
+        RIGHT
+    };
+    Orientation orientation = RIGHT;
     while(!done())
     {
-
-
+        delay(10);
+        if (is_pressed(VSGL_RIGHT)){
+            move_animation(hero, 1, 0);
+            if (orientation == LEFT){
+                toggle_flip_h_animation(hero);
+                orientation = RIGHT;
+            }
+        }
+        if (is_pressed(VSGL_LEFT)){
+            if (orientation == RIGHT){
+                toggle_flip_h_animation(hero);
+                orientation = LEFT;
+            }
+            move_animation(hero, -1, 0);
+        }
         update();
     }
-
     //close the library and clean up everything
     close();
     return 0;
